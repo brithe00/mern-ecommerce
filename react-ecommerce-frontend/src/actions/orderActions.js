ed8605e577ad9ffcad83342a2e961ca094ea23f8
Brian <brian@pop-os.localdomain>
2021-02-10 20:04:36 +0100

    fixed bugs: cart and input are the same for everyone

--- react-ecommerce-frontend/src/actions/orderActions.js
+++ react-ecommerce-frontend/src/actions/orderActions.js
@@ -13,6 +13,7 @@ import {
   ORDER_LIST_MY_SUCCESS,
   ORDER_LIST_MY_FAIL,
 } from "../constants/orderConstants";
+import { CART_CLEAR_ITEMS } from "../constants/cartConstants";
 
 export const createorder = (order) => async (dispatch, getState) => {
   try {
@@ -37,6 +38,12 @@ export const createorder = (order) => async (dispatch, getState) => {
       type: ORDER_CREATE_SUCCESS,
       payload: data,
     });
+
+    dispatch({
+      type: CART_CLEAR_ITEMS,
+      payload: data,
+    });
+    localStorage.removeItem("cartItems");
   } catch (error) {
     dispatch({
       type: ORDER_CREATE_FAIL,
--- react-ecommerce-frontend/src/actions/userActions.js
+++ react-ecommerce-frontend/src/actions/userActions.js
@@ -16,6 +16,7 @@ import {
   USER_DETAILS_RESET,
 } from "../constants/userConstants";
 import { ORDER_LIST_MY_RESET } from "../constants/orderConstants";
+import { CART_CLEAR_ITEMS } from "../constants/cartConstants";
 
 export const login = (email, password) => async (dispatch) => {
   try {
@@ -54,9 +55,14 @@ export const login = (email, password) => async (dispatch) => {
 
 export const logout = () => (dispatch) => {
   localStorage.removeItem("userInfo");
+  localStorage.removeItem("cartItems");
+  localStorage.removeItem("shippingAddress");
+  localStorage.removeItem("paymentMethod");
   dispatch({ type: USER_LOGOUT });
   dispatch({ type: USER_DETAILS_RESET });
   dispatch({ type: ORDER_LIST_MY_RESET });
+  dispatch({ type: CART_CLEAR_ITEMS });
+  document.location.href = "/login";
 };
 
 export const register = (name, email, password) => async (dispatch) => {
